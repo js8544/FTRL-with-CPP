a70020cfdcbb8651c45c7e665720d2e517eef6bb
jinshang <jinshang@tencent.com>
2019-06-24 22:00:36 +0800

    add ranking/prediction support add inter-process communication via shared memory

---
+++ src/corpus.cpp
@@ -0,0 +1,46 @@
+#include "corpus.h"
+using namespace std;
+
+vector<string> parse_feature(string s, const string del){//same as python string.split(string, delimiter)
+	size_t pos = 0;
+	string token;
+	vector<string> ret;
+
+	while((pos = s.find(del))!=string::npos){
+		token = s.substr(0,pos);
+		ret.push_back(token);
+		s.erase(0,pos+del.size());
+	}
+	ret.push_back(s);
+	return ret;
+}
+
+corpus::corpus(ifstream* File){//file is of form "y x1:v1 x2:v2 ..."
+	file = File;
+	d = 0;
+	string line;
+	while(getline(*file,line)){
+		vector<string> f = parse_feature(line," ");
+
+		int y = (f[0]=="1")?1:0;
+		SpVec fv;
+		for(int i = 1;i < f.size();i++){
+			
+			vector<string> temp = parse_feature(f[i],":");
+			
+			long x = stoi(temp[0]);
+			double v = stod(temp[1]);
+			fv.set_value(x,v);
+			if(x>d) d = x;
+		}
+		data.push_back(data_str(fv,y));
+	}
+}
+
+long corpus::size(){
+	return data.size();
+}
+
+data_str &corpus::operator[](long i){
+	return data[i];
+}
